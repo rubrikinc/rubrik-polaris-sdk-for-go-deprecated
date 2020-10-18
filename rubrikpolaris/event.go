3a5dd101f2197b91f7f73cead0061f46915472c3
drew-russell <drew.russell@rubrik.com>
2020-10-18 04:26:59 +0000

    add events

---
+++ rubrikpolaris/event.go
@@ -0,0 +1,43 @@
+package rubrikpolaris
+
+import "time"
+
+func (c *Credentials) GetAllEvents(secondsTimeRange int, timeout ...int) (interface{}, error) {
+
+	httpTimeout := httpTimeout(timeout)
+
+	query, err := c.readQueryFile("AllEventsPerTimePeriod.graphql")
+	if err != nil {
+		return 0, err
+	}
+
+	variables := map[string]interface{}{}
+	variables["timeAgo"] = time.Now().Add(time.Duration(secondsTimeRange*-1) * time.Second).UTC().Format(time.RFC3339)
+
+	events, err := c.QueryWithVariables(query, variables, httpTimeout)
+	if err != nil {
+		return 0, err
+	}
+	return revents, nil
+
+}
+
+func (c *Credentials) GetAllAuditLogByMinute(minuteTimeRange int, timeout ...int) (interface{}, error) {
+
+	httpTimeout := httpTimeout(timeout)
+
+	query, err := c.readQueryFile("AllAuditLogPerTimePeriod.graphql")
+	if err != nil {
+		return 0, err
+	}
+
+	variables := map[string]interface{}{}
+	variables["timeAgo"] = time.Now().Add(time.Duration(minuteTimeRange*-1) * time.Minute).UTC().Format(time.RFC3339)
+
+	eventLog, err := c.QueryWithVariables(query, variables, httpTimeout)
+	if err != nil {
+		return 0, err
+	}
+	return eventLog, nil
+
+}
--- rubrikpolaris/types.go
+++ rubrikpolaris/types.go
@@ -1,5 +1,7 @@
 package rubrikpolaris
 
+import "time"
+
 type RadarEnabledClusters struct {
 	Data struct {
 		RadarClusterConnection struct {
@@ -9,5 +11,44 @@ type RadarEnabledClusters struct {
 				Name         string      `mapstructure:"name"`
 			} `mapstructure:"nodes"`
 		} `mapstructure:"radarClusterConnection"`
+	} `mapstructure:"data"`
+}
+
+type AllEvent struct {
+	Data struct {
+		ActivitySeriesConnection struct {
+			Edges []struct {
+				Node struct {
+					ID                   int         `mapstructure:"id"`
+					Fid                  string      `mapstructure:"fid"`
+					ActivitySeriesID     string      `mapstructure:"activitySeriesId"`
+					LastUpdated          time.Time   `mapstructure:"lastUpdated"`
+					LastActivityType     string      `mapstructure:"lastActivityType"`
+					LastActivityStatus   string      `mapstructure:"lastActivityStatus"`
+					ObjectID             string      `mapstructure:"objectId"`
+					ObjectName           string      `mapstructure:"objectName"`
+					ObjectType           string      `mapstructure:"objectType"`
+					Severity             string      `mapstructure:"severity"`
+					Progress             interface{} `mapstructure:"progress"`
+					IsCancelable         interface{} `mapstructure:"isCancelable"`
+					IsPolarisEventSeries bool        `mapstructure:"isPolarisEventSeries"`
+					Cluster              struct {
+						ID   string `mapstructure:"id"`
+						Name string `mapstructure:"name"`
+					} `mapstructure:"cluster"`
+					ActivityConnection struct {
+						Nodes []struct {
+							ID      string `mapstructure:"id"`
+							Message string `mapstructure:"message"`
+						} `mapstructure:"nodes"`
+					} `mapstructure:"activityConnection"`
+				} `mapstructure:"node"`
+			} `mapstructure:"edges"`
+			PageInfo struct {
+				EndCursor       string `mapstructure:"endCursor"`
+				HasNextPage     bool   `mapstructure:"hasNextPage"`
+				HasPreviousPage bool   `mapstructure:"hasPreviousPage"`
+			} `mapstructure:"pageInfo"`
+		} `mapstructure:"activitySeriesConnection"`
 	} `mapstructure:"data"`
 }
